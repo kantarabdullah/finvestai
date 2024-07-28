e5f686fda009745d5d0fc07c9de740afbe32a9fc
kantarabdullah <87432586+kantarabdullah@users.noreply.github.com>
2024-07-28 22:49:51 +0300

    Update prompt

--- src/app/api/chat-gpt/route.js
+++ src/app/api/chat-gpt/route.js
@@ -13,47 +13,32 @@ export async function POST(request) {
     messages: [
       {
         role: 'system',
-        content: `Prentend your are a Financial Expert with Stock Rating Insights. You have tasks about stock that is ${params.prompt}, and these steps are:
+        content: `Pretend you are a Financial Expert with Stock Rating Insights. You have tasks related to the stock ${params.prompt}, and these steps are:
 
-        Step 1: Search for the news about ${params.prompt} for about 1 month which are important and impactful for ${params.prompt} stock price for short term, 
-        search news from especialy Yahoofinance and bloomberg.
+        Step 1: Search the web for news and data such as indicators, market trends, financial reports, industry news, and broader economic factors about ${params.prompt}.
 
-        Step 2: If there is notable positive or negative news categorize them for "Positive News" that affect the stock price to increase, 
-        and "Negative News" that affect the stock price to decrease. And summarize them.
+        Step 2: If there is notable data for ${params.prompt}, categorize them into "Strengthening Signals" that indicate the stock price may increase, 
+        and "Weakening Signals" that indicate the stock price may decrease. Write a paragraph about the collected data and its effect on ${params.prompt}.
 
-        Step 3: Analyze the ${params.prompt} stock with the data ${params.stockData}, perform a technical analysis like an financial expert.
+        Step 3: Analyze the ${params.prompt} stock with the data ${params.stockData}. Perform a technical analysis as a financial expert would.
 
-        Step 4: With the result from previous step, you will providing a rating from 1 to 10 for ${params.prompt} for short term. This rating system is designed 
-        with 5 as a neutral point, indicating no significant expected change in the stock price. A rating closer to 1 suggests a strong 
-        bearish outlook, predicting that the company's stock price is likely to decrease. Conversely, a rating closer to 10 indicates 
-        a bullish outlook, suggesting that the stock price is expected to increase.
-        
-        
         Format:
         <div class="text-white border bg-violet-500/25 border-violet-500 p-8">
           <h1 class="text-3xl">Stock Analysis for ${params.prompt}</h1>
           
-          <h2 class="mt-8 mb-2 text-green-500 text-2xl">Positive News</h2>
-          <ul class="positive-news">
-            <!-- Insert positive news items here -->
-            <li class="mb-1"><span class="news">News: [Summary of Positive News for ${params.prompt}]</span><br/>
-            <span class="text-slate-500 font-bold text-sm">Resource: [Resource]</span></li>
-          </ul>
+          <h2 class="mt-8 mb-2 text-green-500 text-2xl">Strengthening Signals</h2>
+          <p class="mb-1">
+            [Strengthening Signals for ${params.prompt}]
+          </p>
           
-          <h2 class="mt-4 mb-2 text-red-500 text-2xl">Negative News</h2>
-          <ul class="negative-news">
-            <!-- Insert negative news items here -->
-            <li class="mb-1"><span class="news">News: [Summary of Negative News for ${params.prompt}]</span><br/>
-            <span class="text-slate-500 font-bold text-sm">Resource: [Resource]</span></li>
-          </ul>
+          <h2 class="mt-4 mb-2 text-red-500 text-2xl">Weakening Signals</h2>
+          <p class="mb-1">
+            [Weakening Signals for ${params.prompt}]
+          </p>
           
           <h2 class="mt-4 mb-2 text-2xl">Technical Analysis</h2>
-          <p class="technical-analysis">[Technical analysis with ${params.stockData} write like a financial advisor]</p>
-
-          <h2 class="mt-4 mb-2 text-2xl">Rating</h2>
-          <p class="rating">[Short term rating for ${params.prompt} is x/10]</p>
-        </div>
-        `,
+          <p class="technical-analysis">[Technical analysis with ${params.stockData} written like a financial advisor]</p>
+        </div>`,
       },
       {
         role: 'user',
@@ -61,7 +46,7 @@ export async function POST(request) {
       },
     ],
     temperature: 1,
-    max_tokens: 800,
+    max_tokens: 1000,
   })
 
   return NextResponse.json(response)
